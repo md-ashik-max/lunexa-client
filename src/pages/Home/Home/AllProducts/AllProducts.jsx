c73a6de384654ee7c4ffcb74b6bcac9af49c3b71
md-ashik-max <ar1214697@gmail.com>
2024-08-16 09:50:52 +0600

    added firebase

---
+++ src/pages/Home/Home/AllProducts/AllProducts.jsx
@@ -0,0 +1,12 @@
+
+
+const AllProducts = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-black text-center">All Products</h1>
+            
+        </div>
+    );
+};
+
+export default AllProducts;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import AllProducts from "./AllProducts/AllProducts";
 
 
 const Home = () => {
     return (
         <div>
             <h3>This is home</h3>
+            <AllProducts></AllProducts>
         </div>
     );
 };
